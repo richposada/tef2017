274ba4238763b2785a33e0d688ad9c396a7ec759
Rich Posada <richposada@gmail.com>
2017-03-29 18:31:54 -0400

    Add project files.

---
+++ TEF/Controllers/HomeController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TEF.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View("Index");
+        }
+
+        public ActionResult Mission()
+        {
+            return View();
+        }
+        public ActionResult Evan()
+        {
+            return View();
+        }
+
+        public ActionResult Neuroblastoma()
+        {
+            return View();
+        }
+
+        public ActionResult Donate()
+        {
+            return View();
+        }
+
+        public ActionResult Help()
+        {
+            return View();
+        }
+
+        public ActionResult Treats()
+        {
+            return View();
+        }
+
+        public ActionResult Leadership()
+        {
+            return View();
+        }
+
+        public ActionResult Research()
+        {
+            return View();
+        }
+
+        public ActionResult News()
+        {
+            return View("Golf");
+        }
+
+        
+    }
+}
---
+++ TEF/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TEF.MvcApplication" Language="C#" %>
